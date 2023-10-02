c06b5f94111da3d9995f6c5f465948293d782bbf
sjain226 <jain.sohalliya@outlook.com>
2023-10-01 21:37:39 -0500

    Added error handling

--- app.py
+++ app.py
@@ -5,53 +5,70 @@ balances = {}
 
 @app.route('/add', methods=['POST'])
 def add():
-    data = request.json
-    payer = data['payer']
-    points = data['points']
-    timestamp = data['timestamp']
+    try:
+        data = request.json
+        payer = data['payer']
+        points = data['points']
+        timestamp = data['timestamp']
 
-    transactions.append({
-        'payer': payer,
-        'points': points,
-        'timestamp': timestamp
-    })
-    balances[payer] = balances.get(payer, 0) + points
-    return '', 200
+        if not points or not payer or not timestamp:
+            return jsonify({"error" : "Invalid request data, check request body"}), 400
+
+        if points<0:
+            return jsonify({"error" : "Points must be greater than 0"}), 400
+
+        transactions.append({
+            'payer': payer,
+            'points': points,
+            'timestamp': timestamp
+        })
+        balances[payer] = balances.get(payer, 0) + points
+        return '', 200
+    
+    except Exception as e:
+        return jsonify({"error": stre(e)}), 500
 
 @app.route('/spend', methods=['POST'])
 def spend():
-    data = request.json
-    points_spent = data['points']
-    result = []
-    total_balance = 0
-    sort = sorted(transactions, key=lambda x: x['timestamp'])
-    for transaction in sort:
-        total_balance += transaction['points']
+    try:
+        data = request.json
+        points_to_spend = data['points']
+        result = []
+        total_balance = 0
+        sort = sorted(transactions, key=lambda x: x['timestamp'])
 
-    if total_balance < points_spent:
-        return 'User does not have enough points', 400
+        for transaction in sort:
+            total_balance += transaction['points']
 
-    for transaction in sort:
-        payer = transaction['payer']
-        points = transaction['points']
+        if total_balance < points_to_spend:
+            return jsonify({"error" : "User does not have enough points"}), 400
 
-        if points_spent == 0:
-            break
+        for transaction in sort:
+            payer = transaction['payer']
+            points = transaction['points']
 
-        if balances.get(payer, 0) > 0:
-            spent = min(points_spent, points)
-            balances[payer] -= spent
-            points_spent -= spent
-            result.append({'payer': payer, 'points': -spent})
+            if points_to_spend == 0:
+                break
 
-    if points_spent > 0:
-        return 'User does not have enough points', 400
+            if balances.get(payer, 0) > 0:
+                spent = min(points_to_spend, points)
+                balances[payer] -= spent
+                points_to_spend -= spent
+                result.append({'payer': payer, 'points': -spent})
 
-    return jsonify(result), 200
+        return jsonify(result), 200
+
+    except Exception as e:
+        return jsonify({"error" : stre(e)}), 500
 
 @app.route('/balance', methods=['GET'])
 def balance():
-    return jsonify(balances), 200
+    try:
+        return jsonify(balances), 200
+
+    except Exception as e:
+        return jsonify({"error" : stre(e)}), 500
+
 
 
 if __name__ == '__main__':
